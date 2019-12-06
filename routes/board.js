495219bc392175f0cc326c2ffa7831c98ad28df7
Wonjong <jwj0320@ajou.ac.kr>
2019-12-06 22:09:09 +0900

    discompleted commit

--- routes/board.js
+++ routes/board.js
@@ -4,30 +4,24 @@ var database = require("../lib/database");
 
 router.get('/', async function (req, res) {
     try {
-        const boards = await database.findMultipleListings("boards");
-        res.json(boards);
+        let collections = await database.listCollections();
+        let boards=[];
+        collections.forEach(collection=>{
+            if(collection.name.includes("board")){
+                boards.push(collection);
+            }
+        });
+        console.log(boards);
+        res.end();
+        //res.json(boards);
     } catch (e) {
         console.log(e);
         res.json({ success: false });
     }
 });
-
-router.get("/:idx/p_id",async function(req,res){
-    try{
-        const p_id=req.params.p_id;
-        const boards=await database.findMultipleListings("board"+req.params.idx);
-        res.json({
-            text:boards.slice((p_id-1)*10,p_id*10),
-            text_length:boards.length
-        });
-    }catch(e){
-        res.json({success:false});
-    }
-
-});
 router.post('/:idx/register', async function (req, res) {
     try {
-        const insertedId=await database.createListing("boards"+req.params.idx,req.body);
+        const insertedId=await database.createListing("board"+req.params.idx,req.body);
         await database.pushElementInListing(
             "users",{id:req.user.id},{texts:insertedId}
         );
@@ -38,9 +32,26 @@ router.post('/:idx/register', async function (req, res) {
     }
 });
 
+router.get("/:idx/:p_id",async function(req,res){
+    try{
+        
+        const p_id=req.params.p_id;
+        const boards=await database.findMultipleListings("board"+req.params.idx);
+        data={
+            text:boards.slice((p_id-1)*10,p_id*10),
+            text_length:boards.length
+        };
+        console.log(data);
+        res.json(data);
+    }catch(e){
+        res.json({success:false});
+    }
+
+});
+
 router.get('/:idx', async function (req, res) {
     try {
-        const board = await database.findOneListing("boards", { idx: req.user.idx });
+        const board = await database.findOneListing("board", { idx: req.user.idx });
         res.json(board);
     } catch (e) {
         res.json({ success: false });
